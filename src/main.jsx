fe0882a83642d2bd0656bd9fc6678f666b6287e2
Muhammadilhamakb <111202214109@mhs.dinus.ac.id>
2024-10-14 14:48:38 +0700

    login

--- src/main.jsx
+++ src/main.jsx
@@ -13,24 +13,33 @@
 //     </BrowserRouter>
 //   );
 // };
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-export default App;
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './Login'; 
-import Signup from './Signup'; 
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+// export default App;
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Login from './Login'; 
+// import Signup from './Signup'; 
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Signup" element={<Signup />} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/Login" element={<Login />} />
+//         <Route path="/Signup" element={<Signup />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// };
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
